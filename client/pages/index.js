00af8f79b2bebc189d4685358afba78f8d22a1a3
petrovengineer <37839368+petrovengineer@users.noreply.github.com>
2021-04-01 13:18:08 +0300

    add deleted

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({products, items}) {
           const typeParameter = product.parameters.find(p=>(p.name==='Тип'));
           if(typeParameter){
           const extItems = items.filter(i=>typeParameter.items.find(tpi=>tpi._id===i._id))
-          console.log("EXT ITEMS ",extItems)
+          typeParameter.items = extItems;
           typeParameter.items.map(item=>(
             !sorted[item._id]?sorted[item._id]={name: item.value, sort: item.sort?+item.sort:999, data: [product]}
             :sorted[item._id].data = [...sorted[item._id].data, product]
@@ -32,7 +32,7 @@ export default function Home({products, items}) {
             <a className="anchor" id={_id} href="#"></a>
             <h5 className='mt-3 group'>
               {sorted[_id].name}
-            </h5>          
+            </h5>
             <div className='row mt-4'>
               {sorted[_id].data.map((product, i)=><Product key={i+'pr'} product = {product} items={items} />)}
             </div>
