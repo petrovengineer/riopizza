362a2c72ccf016728ec38ae6bc1d8dc7778259d1
petrovengineer <37839368+petrovengineer@users.noreply.github.com>
2021-04-05 15:15:31 +0300

    fix design

--- client/components/product.js
+++ client/components/product.js
@@ -10,7 +10,7 @@ export default function Product({product}){
         setPrice(product.parameters.find(p=>(p.name==='Цена')));
     }, [])
     return (
-        <div className='col-sm-6 col-md-6 col-lg-4 col-xl-3 rounded '>
+        <div className='col-sm-6 col-md-6 col-lg-4 col-xl-3 px-5 px-sm-3 rounded '>
             <div className='shadow'>
             <img src={img && img.data?`data:image/jpeg;base64,${img.data}`:'/images/pizza.jpg'} className='w-100'/>
             <div className='product p-5 p-sm-4'>
@@ -18,21 +18,24 @@ export default function Product({product}){
                     <h5 className='mb-0' style={{minHeight:'50px'}}>
                         <Link href={'/products/'+_id}>{name}</Link><br/>
                     </h5>
-                    <div style={{wordWrap:'break-word'}} className='mb-4'>
+                    {/* <div style={{wordWrap:'break-word'}} className='mb-4'>
                         {composition && composition.items && composition.items.map((item,i)=>(
                             <span className='mr-2 composition' key={'item'+i}>{item.value}</span>
                         ))}
+                    </div> */}
+                    <div style={{wordWrap:'break-word'}} className='mb-4' style={{fontSize:'12px'}}>
+                        {product.description}
                     </div>
                     <div className='d-flex justify-content-between '>
                         <span className='mt-2' style={{fontSize:'24px', fontWeight:'900'}}>{price && price.value+''}
                             <i className="fas fa-ruble-sign ml-1" style={{fontWeight:'900', fontSize:'22px'}}></i>
                         </span>
                         <div className="cart-opt">
-                                <a href="#">
+                                {/* <a href="#">
                                     <span className='mr-2'>
                                         <i className="fas fa-heart"></i>
                                     </span>
-                                </a>
+                                </a> */}
                                 <Link href={'/products/'+_id}>
                                         <a href="#">
                                             <span>
